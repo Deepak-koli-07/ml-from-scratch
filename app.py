a578b508b3e1a790379f60fd5a70269cf828dafd
Deepak-koli-07 <kolideepak8527@gmail.com>
2025-11-05 00:29:51 +0530

    added title and write

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title = 'Hello just for practice', page_icon = "👋", layout = 'wide')
+st.title("👋 Hello, just for practice")
+st.write("If you can read this, Streamlit is working.")
