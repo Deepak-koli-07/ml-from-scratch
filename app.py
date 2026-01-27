a3faffdfe9509d041002e30ba9ac5111c477be66
Deepak-koli-07 <kolideepak8527@gmail.com>
2026-01-27 19:12:45 +0530

    Delete app.py and requirements.txt

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-st.set_page_config(page_title = 'Hello just for practice', page_icon = "👋", layout = 'wide')
-st.title("👋 Hello, just for practice")
-st.write("If you can read this, Streamlit is working.")
